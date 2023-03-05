dca97faa7e85aac8657107e7d9454af064426273
Jimmy Ayivor <jimmyayivor@gmail.com>
2023-03-05 18:00:23 -0500

    update user quries

--- queries/usersQueries.js
+++ queries/usersQueries.js
@@ -1,16 +1,23 @@
-const usersData = require('../usersData.json');
+const  db  = require('../db/dbConfig.')
 
-const idToUserIndex = usersData.reduce((index, user) => {
-  const { id } = user;
-  index[id] = user;
-  return index;
-}, {});
+const getAllUsers = async () => {
+  try {
+    const allUsers = await db.any(`SELECT * from users`);
+    return allUsers;
+  } catch (err) {
+    return err;
+  }
+};
 
-const getAllUsers = () => usersData;
 
-const getUserById =(id) => {
-  return idToUserIndex[id];
-}
+const getUserById = async (id) => {
+  try {
+    const user = await db.one(`SELECT * from users WHERE users.id=$1`, id);
+    return user;
+  } catch (err) {
+    return err;
+  }
+};
 
 module.exports = {
   getAllUsers,
