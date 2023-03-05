a4792c97146176c7f517f89b0899ca601be242d5
Antonio Shivers <antonioshivers2500@yahoo.com>
2023-03-05 14:09:11 -0500

    completed assessment

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,13 +2,22 @@ const pgp = require("pg-promise")();
 
 require("dotenv").config();
 
-const databaseUrl = process.env.DB_URL;
+const { DB_URL, PG_HOST, PG_DATABASE, PG_USER, PG_PASSWORD, PG_PORT } =
+  process.env;
 
-const cn = {
-  connectionString: databaseUrl,
-  allowExitOnIdle: true,
-  max: 30,
-};
+const cn = DB_URL
+  ? {
+      connectionString: DB_URL,
+      allowExitOnIdle: true,
+      max: 30,
+    }
+  : {
+      host: PG_HOST,
+      database: PG_DATABASE,
+      user: PG_USER,
+      password: PG_PASSWORD,
+      port: PG_PORT,
+    };
 
 const db = pgp(cn);
 
---
+++ db/seed.sql
@@ -0,0 +1,102 @@
+INSERT INTO users (age, name, company, country, photo, about, latitude, longitude)
+VALUES
+(
+  35,
+  'Park Daugherty',
+  'BUZZWORKS',
+  'Greenland',
+  'https://randomuser.me/api/portraits/lego/0.jpg',
+  'Hi there! I''m Park Daugherty, a 35-year-old from Greenland. I work at BUZZWORKS, where I''m passionate about what I do and always strive to exceed expectations. In my free time, I love to spend time with my loved ones and take on new adventures. I''m excited to see what the future holds and what opportunities lie ahead.',
+  '38.680966',
+  '-70.369272'
+),
+(
+  29,
+  'Meagan Ashley',
+  'ASSISTIX',
+  'Romania',
+  'https://randomuser.me/api/portraits/lego/1.jpg',
+  'Meagan is currently employed at ASSISTIX, where she has quickly established herself as a valuable member of the team. Meagan''s strong work ethic, attention to detail, and creative problem-solving skills have made her a sought-after colleague. Outside of work, Meagan enjoys exploring her city, trying new foods, and spending time with her friends and family. She is a well-rounded individual who is always looking for new opportunities to grow and learn.',
+  '39.359043',
+  '-140.795149'
+),
+(
+  29,
+  'Denise Garrison',
+  'VURBO',
+  'Nepal',
+  'https://randomuser.me/api/portraits/lego/2.jpg',
+  'I work at Vurbo, where I get to put my skills to the test every day and help drive the company''s success. Outside of work, I love to immerse myself in the local culture and take in all that Nepal has to offer. I''m also an avid traveler and enjoy experiencing new sights, sounds, and tastes.',
+  '32.675183',
+  '-32.736304'
+),
+(
+  24,
+  'Carroll Melton',
+  'TURNABOUT',
+  'Bulgaria',
+  'https://randomuser.me/api/portraits/lego/3.jpg',
+  'Eu do dolor occaecat nulla quis qui. Sint amet dolor nostrud est minim velit aute et. Cupidatat reprehenderit fugiat commodo laboris tempor eu. Reprehenderit incididunt enim nulla qui.\r\n',
+  '-5.083',
+  '-155.696156'
+),
+(
+  28,
+  'Brigitte Ortega',
+  'INSURITY',
+  'Niue',
+  'https://randomuser.me/api/portraits/lego/4.jpg',
+  'Ex fugiat mollit nisi consequat nostrud mollit dolor ad officia culpa eiusmod reprehenderit ullamco. Velit eu consequat dolore cupidatat excepteur esse qui pariatur adipisicing reprehenderit sunt nisi in. Veniam excepteur cillum proident nostrud minim aliqua ut velit. Duis enim excepteur proident ullamco sint non culpa sunt sit duis. Cupidatat duis veniam enim sint pariatur occaecat id.\r\n',
+  '72.881335',
+  '-147.668845'
+),
+(
+  32,
+  'Keisha Clements',
+  'GEOFORM',
+  'Tuvalu',
+  'https://randomuser.me/api/portraits/lego/5.jpg',
+  'Sint est incididunt deserunt occaecat quis elit enim in labore. Ipsum dolore ipsum labore sit qui velit adipisicing. Ipsum mollit qui Lorem dolor exercitation excepteur nostrud tempor. Tempor incididunt ipsum anim cupidatat do amet adipisicing ea tempor labore incididunt labore. Velit officia enim amet cillum veniam consequat laboris ea anim pariatur. Sint ex aliquip cillum cillum dolor.\r\n',
+  '83.304345',
+  '-104.973509'
+),
+(
+  24,
+  'Rogers Wiley',
+  'POOCHIES',
+  'Senegal',
+  'https://randomuser.me/api/portraits/lego/6.jpg',
+  'Quis sunt irure exercitation veniam aute et proident reprehenderit irure adipisicing esse ad commodo. Anim enim elit dolor excepteur labore id cillum est. Eiusmod laboris adipisicing ad irure enim do sunt. Adipisicing veniam consequat ad sit ut aliquip.\r\n',
+  '0.040734',
+  '-165.031747'
+),
+(
+  21,
+  'Bernard Cruz',
+  'TRANSLINK',
+  'Mozambique',
+  'https://randomuser.me/api/portraits/lego/7.jpg',
+  'Elit qui velit adipisicing sit ut sunt. Officia aliqua ex irure eu magna nisi minim pariatur proident minim aliquip minim. Id elit consequat nulla et ipsum do pariatur irure. Nisi consectetur mollit ut sit aliqua ea duis enim ullamco sit exercitation veniam.\r\n',
+  '-10.959657',
+  '-9.708998'
+),
+(
+  33,
+  'Wong Pate',
+  'KLUGGER',
+  'Tajikistan',
+  'https://randomuser.me/api/portraits/lego/8.jpg',
+  'Dolor sit nulla ipsum laboris ut tempor minim ad consectetur proident velit nisi occaecat. Laborum commodo veniam aliquip non laboris do fugiat in anim sunt veniam aliqua deserunt. Ipsum fugiat cupidatat laborum mollit nisi veniam cupidatat laborum. Eiusmod fugiat culpa tempor irure do. Labore culpa anim veniam aliquip nostrud in non deserunt minim adipisicing et ad ipsum. Consequat nisi laborum qui id magna ipsum labore. Ipsum Lorem non dolore ea anim ex in mollit mollit qui.\r\n',
+  '19.605271',
+  '-106.281246'
+),
+(
+  30,
+  'Felecia Meadows',
+  'LOVEPAD',
+  'El Salvador',
+  'https://randomuser.me/api/portraits/lego/9.jpg',
+  'Voluptate cillum culpa labore ut. Labore nulla consectetur fugiat deserunt excepteur velit deserunt duis proident incididunt voluptate et. Proident qui qui do excepteur consectetur nostrud id nulla excepteur commodo veniam mollit dolor adipisicing. Consequat exercitation enim sint mollit nostrud consequat dolore pariatur magna pariatur. Laboris excepteur voluptate eu ipsum sunt esse dolor elit amet et sunt minim.\r\n',
+  '83.252308',
+  '-124.303889'
+);
