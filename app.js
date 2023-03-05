8adce39aa7ba911fe0f48ccd26fdd7c591368906
Jorge Carrera <JCarrera994@gmail.com>
2023-03-05 14:26:21 -0500

    Refactors controllers and queries files
    The queries and controllers have been updated to serve data from db.
    Removes the data.json file and the test route.

--- app.js
+++ app.js
@@ -13,17 +13,6 @@ app.get('/', (req, res) => {
 
 app.use('/users', usersController);
 
-//TODO: REMOVE THIS TEST ROUTE 
-app.get("/tests", async (request, response) => {
-  try {
-    const tests = await db.any("SELECT * FROM tests");
-    
-    response.status(200).json({ data: tests });
-  } catch (err) {
-    response.status(500).json({ error: err.message });
-  }
-});
-
 app.all('*', (req, res) => {
   res.status(404).json({ error: "Not found"});
 })
