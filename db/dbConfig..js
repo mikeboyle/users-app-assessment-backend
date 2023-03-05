20376e82e581d666ca64942d75c0602280ffd7d0
Jimmy Ayivor <jimmyayivor@gmail.com>
2023-03-05 14:29:03 -0500

    update db config

--- db/dbConfig..js
+++ db/dbConfig..js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 // http://vitaly-t.github.io/pg-promise/module-pg-promise.html
 const pgp = require("pg-promise")();
 require("dotenv").config();
@@ -31,6 +32,30 @@ const cn = {
   user: process.env.PG_USER,
 };
 >>>>>>> 5fc63e6 (adds db config, schema, seed files and updates routes and controllers)
+=======
+// http://vitaly-t.github.io/pg-promise/module-pg-promise.html
+const pgp = require("pg-promise")();
+require("dotenv").config();
+
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
+  process.env;
+/// https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+      password: PG_PASSWORD,
+    };
+>>>>>>> 03a00ab (update db config)
 
 const db = pgp(cn);
 
