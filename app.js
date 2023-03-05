58fc90fdfff9f7cbda3187326f9c0e0e46c5eb91
ziqian pan <ziqianpan1998@gmail.com>
2023-03-05 13:27:54 -0500

    Step 3 - create users table and schema and user
    create both users table and schema and seed file for usersData.
    Works locally.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const usersController = require('./controllers/usersController');
 
+const db = require("./db")
+
 const app = express();
 app.use(cors());
 app.use(express.json());
