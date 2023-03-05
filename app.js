3ebf084f53902bbe73aabdb1fddfc5f6813b2448
Jorge Carrera <JCarrera994@gmail.com>
2023-03-05 13:21:21 -0500

    /tests route to test connection to db
    This route will be removed as soon as possible, but
    it confirms that we can connect the API to an external db.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const usersController = require('./controllers/usersController');
+const db = require("./db");
 
 const app = express();
 app.use(cors());
@@ -15,5 +16,16 @@ app.use('/users', usersController);
 app.all('*', (req, res) => {
   res.status(404).json({ error: "Not found"});
 })
+
+app.get("/tests", async (request, response) => {
+  try {
+    const tests = await db.any("SELECT * FROM tests");
+
+    response.status(200).json({ data: tests });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
+});
+
 
 module.exports = app;
