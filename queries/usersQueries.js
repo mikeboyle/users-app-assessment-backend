ab2b9b2410794ca4c8614edbcfdbe931291b884f
JuHaoChen1997 <juhaochen97@gmail.com>
2023-03-05 13:53:59 -0500

    refactor queries and controllers

--- queries/usersQueries.js
+++ queries/usersQueries.js
@@ -1,16 +1,29 @@
-const usersData = require("../usersData.json");
+// const usersData = require("../usersData.json");
+const db = require("../db/index");
 
-const idToUserIndex = usersData.reduce((index, user) => {
-  const { id } = user;
-  index[id] = user;
-  return index;
-}, {});
+// const idToUserIndex = usersData.reduce((index, user) => {
+//   const { id } = user;
+//   index[id] = user;
+//   return index;
+// }, {});
 
-const getAllUsers = () => usersData;
+async function getAllUsers() {
+  try {
+    const users = await db.any("SELECT * FROM users");
+    return users;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const getUserById = (id) => {
-  return idToUserIndex[id];
-};
+async function getUserById(id) {
+  try {
+    const user = await db.one("SELECT * FROM users WHERE id=$1", id);
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = {
   getAllUsers,
