c9b0d90ff9ef30e2514b6f09f29335e27f616290
Jimmy Ayivor <jimmyayivor@gmail.com>
2023-03-06 13:55:52 -0500

    update query and controller files

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,18 +4,21 @@ const { getAllUsers, getUserById } = require('../queries/usersQueries');
 const usersController = express.Router();
 
 usersController.get('/', async (req, res) => {
-
-  users = await getAllUsers();
-  res.status(200).json({ data: users });
+  try {
+    const users = await getAllUsers();
+    res.status(200).json({ data: users });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 usersController.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  user =  await getUserById(id);
-  if (user) {
+  try {
+    const { id } = req.params;
+    const user = await getUserById(id);
     res.status(200).json({ data: user });
-  } else {
-    res.status(404).json({ error: `User with id ${id} not found`});
+  } catch (err) {
+    res.status(404).json({ error: `User with id ${id} not found` });
   }
 })
 
--- queries/usersQueries.js
+++ queries/usersQueries.js
@@ -1,31 +1,14 @@
-//const usersData = require('../usersData.json');
-
-
 const  db  = require('../db/dbConfig.')
-
-// const idToUserIndex = usersData.reduce((index, user) => {
-//   const { id } = user;
-//   index[id] = user;
-//   return index;
-// }, {});
 
 
 const getAllUsers = async () => {
-  try {
-    const allUsers = await db.any(`SELECT * from users`);
-    return allUsers;
-  } catch (err) {
-    return err;
-  }
+  const users = await db.any('SELECT * FROM users');
+  return users;
 };
 
 const getUserById = async (id) => {
-  try {
     const user = await db.one(`SELECT * from users WHERE users.id=$1`, id);
     return user;
-  } catch (err) {
-    return err;
-  }
 };
 
 
